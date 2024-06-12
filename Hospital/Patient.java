68ebd543e642886f471a2c75f9ea4bb61ab42801
HamidAliKalhoro <aleehamid1024@gmail.com>
2024-06-12 16:27:15 +0500

    connected to the database

--- Hospital/Patient.java
+++ Hospital/Patient.java
@@ -24,6 +24,26 @@ public class Patient {
         System.out.println("Enter patient Gender: ");
         String gender = scanner.next();
 
-        
+        try {
+            String query = "INSERT INTO patient (name, age, gender) VALUES (?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, age);
+            preparedStatement.setString(3, gender);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                JOptionPane.showMessageDialog(null,
+                "Patient added successfully :)", null, JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null,
+                "Failed to add Patient :(", null, JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null,"try again",null, JOptionPane.INFORMATION_MESSAGE);
+            }
+
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(null,
+            "Failed to add Patient :(", null, JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
