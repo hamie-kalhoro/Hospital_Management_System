d6ab608e54b9d73e26f7aca5b44c07dfb3a706ff
HamidAliKalhoro <aleehamid1024@gmail.com>
2024-06-12 16:26:12 +0500

    Created an addPatient method to add a patient with the requirements of name, age, and gender.

--- Hospital/Patient.java
+++ Hospital/Patient.java
@@ -1,11 +1,14 @@
 package Hospital;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Patient {
-    Connection connection;
-    Scanner scanner;
+    static Connection connection;
+    static Scanner scanner;
 
     Patient(Connection connection, Scanner scanner) {
         this.connection = connection;
@@ -13,12 +16,34 @@ public class Patient {
     }
 
     public void addPatient() {
-        System.out.println("Add Patient");
+        System.out.println("Add Patient/n");
         System.out.println("Enter patient Name: ");
         String name = scanner.next();
         System.out.println("Enter patient Age: ");
         int age = scanner.nextInt();
         System.out.println("Enter patient Gender: ");
         String gender = scanner.next();
+
+        try {
+            String query = "INSERT INTO patient (name, age, gender) VALUES (?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, age);
+            preparedStatement.setString(3, gender);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                JOptionPane.showMessageDialog(null,
+                "Patient added successfully :)", null, JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null,
+                "Failed to add Patient :(", null, JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null,"try again",null, JOptionPane.INFORMATION_MESSAGE);
+            }
+
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(null,
+            "Failed to add Patient :(", null, JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
