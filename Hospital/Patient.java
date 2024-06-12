2cc99cfd7b9010ae1a53693689b565f947905f8a
HamidAliKalhoro <aleehamid1024@gmail.com>
2024-06-12 16:13:59 +0500

    adding the add patient method in Patient class

--- Hospital/Patient.java
+++ Hospital/Patient.java
@@ -1,4 +1,24 @@
 package Hospital;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
 public class Patient {
-    
+    Connection connection;
+    Scanner scanner;
+
+    Patient(Connection connection, Scanner scanner) {
+        this.connection = connection;
+        this.scanner = scanner;
+    }
+
+    public void addPatient() {
+        System.out.println("Add Patient");
+        System.out.println("Enter patient Name: ");
+        String name = scanner.next();
+        System.out.println("Enter patient Age: ");
+        int age = scanner.nextInt();
+        System.out.println("Enter patient Gender: ");
+        String gender = scanner.next();
+    }
 }
