f4d60182b82455f0db3f331b3492e5f751cbb76d
HamidAliKalhoro <aleehamid1024@gmail.com>
2024-06-13 14:34:56 +0500

    connection is made in doctor class

--- Hospital/Doctor.java
+++ Hospital/Doctor.java
@@ -1,4 +1,16 @@
 package Hospital;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
 public class Doctor {
+
+    Connection connection;
+    Scanner scanner;
+
+    public Doctor(Connection connection, Scanner scanner) {
+        this.connection = connection;
+        this.scanner = scanner;
+    }
     
 }
