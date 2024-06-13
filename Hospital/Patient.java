1d93c61a9e9055556ecf628f98ec81e04f8cbb4d
HamidAliKalhoro <aleehamid1024@gmail.com>
2024-06-13 14:28:34 +0500

    created a getPatientbyId method in Patient class which will return any boolean value whether  a patient is present or not, if the patient is present then retrieve the patient by id else false;

--- Hospital/Patient.java
+++ Hospital/Patient.java
@@ -3,6 +3,7 @@ package Hospital;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
@@ -77,5 +78,20 @@ public class Patient {
 
     public boolean getPatientbyId(int id) {
         String query = "SELECT * FROM Patient WHERE id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return false;
     }
 }
